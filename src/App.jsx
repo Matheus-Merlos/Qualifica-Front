9d0fd773a9142aaf414612acdf2eaee6f177b691
LeonardoSantan <leofelip2001@gmail.com>
2025-06-26 22:41:05 -0300

    feat/api integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import Login from './pages/Login';
-// import Curso from './components/Curso'
-// import Home from './components/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Curso from './pages/Curso';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* rota raiz → tela de login */}
         <Route path="/" element={<Login />} />
+
+        {/* nova rota → tela Home */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/curso" element={<Curso />} />
       </Routes>
     </BrowserRouter>
   );
