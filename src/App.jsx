0719bcb5f582ef6329a5e6aa2c36f7ae778f8172
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-06-28 19:47:44 -0300

    feat: Criação da rota 404

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Curso from './pages/Curso';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Route404 from './pages/404';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* rota raiz → tela de login */}
-        <Route path="/" element={<Login />} />
+        <Route path='/' element={<Login />} />
 
         {/* nova rota → tela Home */}
-        <Route path="/home" element={<Home />} />
-        <Route path="/curso" element={<Curso />} />
-        <Route path="/course/:id" element={<Curso />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/curso' element={<Curso />} />
+        <Route path='/course/:id' element={<Curso />} />
+
+        <Route path='*' element={<Route404 />} />
       </Routes>
     </BrowserRouter>
   );
